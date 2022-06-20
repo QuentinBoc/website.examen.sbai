07836bc550070d5dbd800bd605d5c500ba0af6af
QuentinBoc <92474524+QuentinBoc@users.noreply.github.com>
2022-06-20 15:57:25 +0200

    Add files via upload

---
+++ ContactMe.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.scss" />
+    <script src="https://kit.fontawesome.com/4a9f87c010.js" crossorigin="anonymous"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet" />
+    <title>Contact moi</title>
+</head>
+
+<body>
+    <nav>
+        <div class="profile">
+        </div>
+        <ul>
+            <li><a href="index.html">Acceuil</li></a>
+            <li><a href="MySchool.php">My school</li></a>
+        </ul>
+    </nav>
+
+    <section class="page-content">
+        <header>
+            <h1>Contacte moi :</h1>
+        </header>
+        <form>
+
+
+            <div class="separation"></div>
+            <div class="corps-formulaire">
+                <div class="gauche">
+                    <div class="groupe">
+                        <label>Votre Prénom</label>
+                        <input type="text" autocomplete="off" />
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="groupe">
+                        <label>Votre adresse e-mail</label>
+                        <input type="text" autocomplete="off" />
+                        <i class="fas fa-envelope"></i>
+                    </div>
+                    <div class="groupe">
+                        <label>Votre téléphone</label>
+                        <input type="text" autocomplete="off" />
+                        <i class="fas fa-mobile"></i>
+                    </div>
+                </div>
+
+                <div class="droite">
+                    <div class="groupe">
+                        <label>Message</label>
+                        <textarea placeholder="Saisissez ici..."></textarea>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pied-formulaire" align="center">
+                <button>Envoyer le message</button>
+            </div>
+        </form>
+</body>
+
+</html>
+
+<footer>
+    <div class="footer-content">
+        <ul class="socials">
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-google"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+        </ul>
+    </div>
+    <div class="footer-bottom">
+        <p>copyright &copy;2022 Portefolio. designed by <span>Quentin.</span></p>
+    </div>
+</footer>
+</body>
+
+</html>
---
+++ MySchool.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.scss" />
+    <script src="https://kit.fontawesome.com/4a9f87c010.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+<nav>
+        <div class="profile">
+        </div>
+        <ul>
+            <li><a href="index.html">Acceuil</li></a>
+            <li><a href="ContactMe.php">Contact Me</li></a>
+        </ul>
+    </nav>
+
+    <section class="page-content">
+        <header>
+            <h1>Mon ecole :</h1>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2524.897023503276!2d3.2179309519194685!3d50.740398779415905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3258d512bc8f3%3A0xc53bd8f3cc4f3dae!2sEAFC%20MOUSCRON%20WALLONIE%20PICARDE!5e0!3m2!1sfr!2sbe!4v1655656961297!5m2!1sfr!2sbe" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </header>
+        <footer>
+            <div class="footer-content">
+                <ul class="socials">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+            <div class="footer-bottom">
+                <p>copyright &copy;2022 Portefolio. designed by <span>Quentin.</span></p>
+            </div>
+        </footer>
+</body>
+
+</html>
